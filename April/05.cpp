ddf5d56d59c5c08c6542c3718b7173eb054a6a08
Arunabh0409 <arunabhagarwalindia@gmail.com>
2024-04-05 13:49:53 +0530

    5th Day

---
+++ April/05.cpp
@@ -0,0 +1,32 @@
+// https://leetcode.com/problems/make-the-string-great/
+
+class Solution
+{
+public:
+    // stack solution -- having a track of current and prev char
+    string makeGood(string s)
+    {
+
+        int n = s.length();
+        stack<char> st;
+        string temp = "";
+
+        for (int i = 0; i < n; i++)
+        {
+
+            if (!st.empty() && (st.top() + 32 == s[i] || st.top() - 32 == s[i]))
+                st.pop();
+            else
+                st.push(s[i]);
+        }
+
+        // take out letters from stack and push into the string
+
+        while (!st.empty())
+        {
+            temp = st.top() + temp;
+            st.pop();
+        }
+        return temp;
+    }
+};
